e04d4fa6b7303af86eb07fb9768858b92efa87f5
zdeath102 <76021839+zdeath102@users.noreply.github.com>
2021-03-12 12:22:17 +0700

    feature#4_signup

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -48,14 +48,6 @@ userSchema.pre('save', async function (next) {
     }
     next()
 })
-/*userSchema.methods.generateAuthToken = async function() {
-    // Generate an auth token for the user
-    const user = this
-    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
-    user.tokens = user.tokens.concat({token})
-    await user.save()
-    return token
-}*/
 
 
 export const User = mongoose.model('User', userSchema);
