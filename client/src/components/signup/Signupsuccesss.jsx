0a21d8e213ea29fb2b08dd92d550f7fe469602ee
DESKTOP-PLE7THI\huyan <huyanhdo0302@gmail.com>
2021-03-12 18:03:34 +0700

    Sign up page

---
+++ client/src/components/signup/Signupsuccesss.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+
+const Signupsuccess= () => {
+    return (
+        <h1 className="success">Thank you for registering</h1>
+    )
+}
+
+export default Signupsuccess;
