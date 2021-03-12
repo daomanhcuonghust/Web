c1c22630a5e8d6a415c62942f98bbffa62858ab0
zdeath102 <76021839+zdeath102@users.noreply.github.com>
2021-03-12 11:36:48 +0700

    add feature#4_sign_up

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import {handleError} from '../middlewares';
 import demoRouter from './demo';
-import authenRouter from './authen'
-
+import authenRouter from './authen';
+import userRouter from './signup'
 const router = express.Router();
 
 router.use(demoRouter);
