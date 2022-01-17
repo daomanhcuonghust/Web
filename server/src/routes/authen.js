b51edff2edd07d6cee719a0322cc02a738aee4b6
Lê Thanh Thiên <83025525+MadridMaverick@users.noreply.github.com>
2022-01-18 00:08:57 +0700

    add routes for facilities

--- server/src/routes/authen.js
+++ server/src/routes/authen.js
@@ -1,26 +1,38 @@
-import express from 'express';
-import {checkLogin, logout, createUser,checkLoginStaff,createStaff,
-    CreateTicket,UserBuyTicket,
-    updateTicket,
-    deleteTicket
-} from '../controllers';
-import {validateLogin, validateSignup,validateSignupStaff} from '../middlewares';
+import express from "express"
+import {
+  checkLogin,
+  logout,
+  createUser,
+  checkLoginStaff,
+  createStaff,
+  CreateTicket,
+  UserBuyTicket,
+  updateTicket,
+  deleteTicket,
+  createFacilities,
+  updateFacilities,
+  deleteFacilities,
+} from "../controllers"
+import {
+  validateLogin,
+  validateSignup,
+  validateSignupStaff,
+} from "../middlewares"
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/login', validateLogin, checkLogin);
-router.post('/login-staff', validateLogin, checkLoginStaff);
-router.post('/logout', logout);
-router.post('/signup', validateSignup, createUser);
-router.post('/signup-staff', validateSignupStaff, createStaff);
-router.post('/ticket',  CreateTicket);
-router.patch('/ticket/:id',  updateTicket);
-router.delete('/ticket/:id',  deleteTicket);
+router.post("/login", validateLogin, checkLogin)
+router.post("/login-staff", validateLogin, checkLoginStaff)
+router.post("/logout", logout)
+router.post("/signup", validateSignup, createUser)
+router.post("/signup-staff", validateSignupStaff, createStaff)
+router.post("/ticket", CreateTicket)
+router.patch("/ticket/:id", updateTicket)
+router.delete("/ticket/:id", deleteTicket)
+router.post("/facilities", createFacilities)
+router.patch("/facilities/:id", updateFacilities)
+router.delete("/facilities/:id", deleteFacilities)
 
+router.post("/user-buy-ticket", UserBuyTicket)
 
-router.post('/user-buy-ticket',  UserBuyTicket);
-
-
-
-
-export default router;
+export default router
