e220827813efbc416487d35eafca1ef57831578f
MadridMaverick <83025525+MadridMaverick@users.noreply.github.com>
2022-01-19 22:43:02 +0700

    add user's profile management

--- server/src/controllers/signup.js
+++ server/src/controllers/signup.js
@@ -1,24 +1,20 @@
-import {handleAsync} from '../utils';
-import {UserService} from '../services';
-import { JWT_SECRET } from "../configs";
-import jwt from "jsonwebtoken";
+import { handleAsync } from "../utils"
+import { UserService } from "../services"
+import { JWT_SECRET } from "../configs"
+import jwt from "jsonwebtoken"
 
+export const getUSer = handleAsync(async (req, res) => {
+  const users = await UserService.getAll()
+  res.json({
+    data: users,
+  })
+})
 
-export const getUSer = handleAsync(async(req, res) => {
-    const users = await UserService.getAll();
-    res.json({
-        data: users
-    });
-});
-
-export const createUser = handleAsync(async(req, res) => {
-    const user = await UserService.create(req.body);
-    const accessToken = jwt.sign(
-        user._id,
-        JWT_SECRET
-      );
-    res.json({
-        data: user,
-        accessToken:accessToken
-    });
+export const createUser = handleAsync(async (req, res) => {
+  const user = await UserService.create(req.body)
+  const accessToken = jwt.sign(user._id, JWT_SECRET)
+  res.json({
+    data: user,
+    accessToken: accessToken,
+  })
 })
--- server/src/routes/authen.js
+++ server/src/routes/authen.js
@@ -22,15 +22,12 @@ import {
   createTypeTicket,
   infoTicket,
   updateTypeTicket,
-
   userTicket,
   deleteTypeTickte,
   UserRegisterEvent,
   participantsEvent,
   checkInTicket,
   checkoutTicket,
-
-
   getStaff,
   updateStaff,
   deleteStaff,
@@ -38,7 +35,8 @@ import {
   updateUser,
   deleteUser,
   getIncome,
-
+  getPersonalInfo,
+  personalUpdateUser,
 } from "../controllers"
 import {
   validateLogin,
@@ -56,29 +54,30 @@ router.post("/logout", logout)
 router.post("/signup", validateSignup, createUser)
 router.post("/signup-staff", validateSignupStaff, createStaff)
 
+router.get("/userInfo/:id", getPersonalInfo)
+router.patch("/userInfo/:id", personalUpdateUser)
+
 router.post("/ticket", CreateTicket)
 router.get("/ticket/:id", infoTicket)
 router.patch("/ticket/:id", updateTicket)
 router.delete("/ticket/:id", deleteTicket)
 
-router.put("/typeTicket/:id",createTypeTicket);
-router.delete("/typeTicket/:id/:typeId",deleteTypeTickte);
-router.put("/ticket/:id/:typeId",updateTypeTicket)
-router.get("/ticket/user/:userId",userTicket)
+router.put("/typeTicket/:id", createTypeTicket)
+router.delete("/typeTicket/:id/:typeId", deleteTypeTickte)
+router.put("/ticket/:id/:typeId", updateTypeTicket)
+router.get("/ticket/user/:userId", userTicket)
 
 router.put("/typeTicket/:id", createTypeTicket)
 router.put("/ticket/:id/:typeId", updateTypeTicket)
 router.post("/ticketIncome", validateViewIncome, getIncome)
 
-
 router.post("/facilities", createFacilities)
 router.patch("/facilities/:id", updateFacilities)
-router.post("/userEvent",UserRegisterEvent);
+router.post("/userEvent", UserRegisterEvent)
 router.delete("/facilities/:id", deleteFacilities)
 router.post("/Event", createEvent)
 
-router.get("/event/user/:id",participantsEvent)
-
+router.get("/event/user/:id", participantsEvent)
 
 router.get("/staffs", getStaff)
 router.post("/staffs", createStaff)
@@ -90,7 +89,6 @@ router.post("/users", createUser)
 router.patch("/users/:id", updateUser)
 router.delete("/users/:id", deleteUser)
 
-
 router.get("/event/:id", getOneEvent)
 router.patch("/Event/:id", updateEvent)
 router.delete("/Event/:id", deleteEvent)
@@ -100,6 +98,6 @@ router.get("/allEvent", getAllEvent)
 router.post("/user-buy-ticket", UserBuyTicket)
 router.post("/ticket-vip", signedIn, createVipTicket)
 
-router.put("/staff/checkin",signedIn,checkInTicket);
-router.put("/staff/checkout",signedIn,checkoutTicket);
+router.put("/staff/checkin", signedIn, checkInTicket)
+router.put("/staff/checkout", signedIn, checkoutTicket)
 export default router
