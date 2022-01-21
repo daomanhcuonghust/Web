7f34bc6b364bcd73da992dcd2afcbc005df7e853
phamthao2001 <sat.thu.thao2001@gmail.com>
2022-01-22 01:01:48 +0700

    commit

--- client/src/components/customer/home/Home1.js
+++ client/src/components/customer/home/Home1.js
@@ -4,6 +4,37 @@ import { useNavigate } from 'react-router-dom'
 import './Home1.css'
 
 export default function Home1() {
+    const [lastEvent, setLastEvent] = useState([])
+    const [login, setLogin] = useState(false)
+
+    let navi=useNavigate();
+    
+    const handleEvent=(id)=>{
+        navi(`/user/event/${id}`);
+    }
+
+    useEffect(() => {
+          let accessToken=localStorage.getItem("accessToken");
+          if(accessToken){
+            setLogin(true);
+          } 
+          else setLogin(false);  
+      },[localStorage.getItem("accessToken")])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/v1/latestEvents', {
+                    method: 'GET', // or 'PUT'
+                    }
+            )
+            .then(response => response.json())
+           .then(data => {
+                setLastEvent(data.result);
+            })
+            .catch((error) => {
+                alert("eror");
+            }) 
+      }, []);
+
     return(
         <div className='Home1'>
             <div className='banner'>
@@ -12,8 +43,8 @@ export default function Home1() {
                     <h1 style={{fontWeight: 'bold', color: '#30475E'}}>Chào mừng đến với</h1>
                     <h1 style={{fontWeight: 'bold', color: '#F05454'}}>TinkerBellGarden</h1>
                     <p>Đến với TinkerBellGarden, quý khách sẽ được trải nghiệm hệ thống khu vui chơi trẻ em phong phú, đa dạng, đắm chìm trong cảnh quan thiên nhiên, thưởng thức ẩm thực đa dạng và tận hưởng không khí lễ hội ngập tràn.</p>
-                    <Button style={{backgroundColor: '#F05454', border:'none'}}size="lg">Đặt vé ngay</Button> {' '}
-                    <Button variant="outline-secondary" size="lg">Khám phá</Button>
+                    <Button onClick={()=>navi("/user/tickbooking")} style={{backgroundColor: '#F05454', border:'none'}}size="lg">Đặt vé ngay</Button> {' '}
+                    <Button onClick={()=>navi("/user/event")} variant="outline-secondary" size="lg">Khám phá</Button>
                 </Col>
                 <Col xs={6}><img style={{width:'100%', height:'100%'}} src="https://hoianit.com/wp-content/uploads/2020/06/bana1.jpg"/></Col>
             </Row>
@@ -22,83 +53,23 @@ export default function Home1() {
             <div className='homeComponent'>
                 <h1>Sự kiện mới nhất</h1>
                 <Row>
-                    <Col>
-                    <Card className='homeCard' >
-                        <div className='ribbon'><span>-%50</span></div>
-                        <Card.Img className='homeCardImg' variant="top" src="https://banahills.sunworld.vn/wp-content/uploads/2021/04/DEM-BN-1024x752.jpg" />
-                        <Card.Body className="homeCardBody"> 
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer className='homeCardFooter' style={{borderRadius: '0 0 50px 0px'}}>Xem thêm</Card.Footer>
-                    </Card>
-                    </Col>
-                        
-                    <Col>
-                    <Card className='homeCard' >
-                        <div className='ribbon'><span>-%50</span></div>
-                        <Card.Img className='homeCardImg' variant="top" src="https://banahills.sunworld.vn/wp-content/uploads/2021/04/DEM-BN-1024x752.jpg" />
-                        <Card.Body className="homeCardBody"> 
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer className='homeCardFooter' style={{borderRadius: '0 0 50px 0px'}}>Xem thêm</Card.Footer>
-                    </Card>
-                    </Col>
-
-                    <Col>
-                    <Card className='homeCard' >
-                        <div className='ribbon'><span>-%50</span></div>
-                        <Card.Img className='homeCardImg' variant="top" src="https://banahills.sunworld.vn/wp-content/uploads/2021/04/DEM-BN-1024x752.jpg" />
-                        <Card.Body className="homeCardBody"> 
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer className='homeCardFooter' style={{borderRadius: '0 0 50px 0px'}}>Xem thêm</Card.Footer>
-                    </Card>
-                    </Col>
-                </Row>
-            </div>
-
-            <div className='homeComponent'>
-                <Row>
-                    <Col xs={6} style={{marginLeft:'20px'}}><img style={{width:'100%', height:'100%' , borderRadius:'20px'}} src="https://banahills.sunworld.vn/wp-content/uploads/2018/08/b2-768x511.jpg"/></Col>
-
-                    <Col style={{marginLeft:'100px', marginRight:'60px'}}>
-                        <h1 >Chúng tôi cung cấp các trải nghiệm tốt nhất</h1>
-                        <p>Hệ thống trò chơi đa dạng, cảnh quan thiên nhiên đặc sắc, thưởng thức ẩm thực phong phú và tận hưởng không khí lễ hội ngập tràn là những gì quý khách sẽ được trải nghiệm khi đến với TinkerBellGarden</p>
-                        <Row>
-                            <Col>
-                                <h2 style={{fontWeight: 'bold', color: '#F05454'}}>7</h2>
-                                <h3 style={{fontWeight: 'bold', color: '#676FA3'}}>ngày mở cửa trong tuần</h3>
+                    {
+                        lastEvent.map(evnt=>
+                            <Col key={evnt._id}>
+                                <Card className='homeCard' >
+                                <div className='ribbon'><span>-{evnt.discount}%</span></div>
+                                <Card.Img className='homeCardImg' variant="top" src={evnt.image[0]} />
+                                    <Card.Body className="homeCardBody"> 
+                                        <Card.Title>{evnt.name}</Card.Title>
+                                        <Card.Text>
+                                            {evnt.description}
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer onClick={()=>handleEvent(evnt._id)} className='homeCardFooter' style={{borderRadius: '0 0 50px 0px'}}>Xem thêm</Card.Footer>
+                                </Card>
                             </Col>
-                            <Col>
-                                <h2 style={{fontWeight: 'bold', color: '#F05454'}}>156</h2>
-                                <h3 style={{fontWeight: 'bold', color: '#676FA3'}}>sự kiện trong năm 2021</h3>
-                                
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <h2 style={{fontWeight: 'bold', color: '#F05454'}}>873</h2>
-                                <h3 style={{fontWeight: 'bold', color: '#676FA3'}}>khách trung bình 1 ngày</h3>
-                            </Col>
-                            <Col>
-                                <h2 style={{fontWeight: 'bold', color: '#F05454'}}>1256</h2>
-                                <h3 style={{fontWeight: 'bold', color: '#676FA3'}}>trò chơi</h3>
-                            </Col>
-                        </Row>
-
-                    </Col>
+                        )
+                    }
                 </Row>
             </div>
 
